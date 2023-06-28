e7ae5fb99dc1a03c346a6930c712768bb6704d97
student <valeriibruch@gmail.com>
2023-06-28 17:23:34 +0200

    gen tech hw2

--- genTech_HW2.sql
+++ genTech_HW2.sql
@@ -3,36 +3,33 @@ SELECT Phone
 FROM Suppliers
 WHERE NOT COUNTRY IN ('Germany','France')
 
--- Для товаров до 10 EUR установить категорию 8 и применить наценку в 20%.
-UPDATE Products
-SET
-    CategoryID = 8,
-    Price = Price*1.2
-WHERE Price <= 10
+-- Добавить два произвольных товара.
+INSERT INTO Products(ProductName, Price)
+VALUES
+    ("someProduct_1", 99.99),
+    ("someProduct_2", 90)
 
 -- Удалить поставщиков из USA и Germany.
 DELETE FROM Suppliers
 WHERE
 Country IN ('USA','Germany')
 
---  Добавить два поставщика из Spain с произвольными номерами телефонов.
-INSERT INTO Suppliers(SupplierName, ContactName, Address, City, PostalCode, Country, Phone)
-VALUES
-    (NULL, NULL, NULL, NULL, NULL, 'Spain', +342564784562),
-    (NULL, NULL, NULL, NULL, NULL, 'Spain', +342549965358)
-
 -- Вывести все товары до 20 EUR из категорий 3 и 6 по убыванию цены.
 SELECT * 
-FROM [Products]
+FROM Products
     WHERE Price <= 20
 AND 
     CategoryID IN (3, 6)
 ORDER BY Price DESC 
 
--- Вывести три самых дорогих товара до 200 EUR.
+-- Вывести номера трех последних заказов.
 SELECT * 
-FROM [Products]
-WHERE 
-     Price <= 200
-ORDER BY Price DESC
+FROM Orders
+ORDER BY OrderDate DESC
+LIMIT 3
+
+-- 2 способ, мне кажется более верным
+SELECT * 
+FROM Orders
+ORDER BY OrderID DESC
 LIMIT 3
